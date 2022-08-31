03d831302f7e426128227638032aeee6b62c63dd
Ahmet DALDEMİR <ahmetdaldemir@karaca.com>
2022-08-31 21:09:22 +0300

    xxa

--- app/Http/Controllers/XmlController.php
+++ app/Http/Controllers/XmlController.php
@@ -2,7 +2,13 @@
 
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductContent;
 use App\Models\Xml;
+use App\Models\XmlManufacturer;
+use App\Models\XmlPictures;
+use App\Models\XmlProduct;
+use App\Models\XmlProductCombination;
 use App\Services\ArrayToXml;
 use DOMDocument;
 use Illuminate\Http\Request;
@@ -21,7 +27,7 @@ class XmlController extends Controller
         $xml->save();
     }
 
-
+/*
     public function parser(Request $request)
     {
         header("Content-Type: application/xml; charset=utf-8");
@@ -90,6 +96,116 @@ class XmlController extends Controller
             $book1->addChild("Categories", $a->xpath('Category')[0]->CategorName);
             $book1->addChild("Manufacturers", $b->xpath('Manufacturer')[0]->ManufacturerName);
             $i++;
+        }
+
+        file_put_contents('books.xml', $simplexml->asXML());
+        return redirect()->back();
+    }
+*/
+
+    public function databaseXml()
+    {
+
+        /*  $result = Product::all();
+          foreach ($result as $items) {
+              $categorylists = explode("&", $items->category_tree);
+                  $i = 0;
+                  foreach ($categorylists as $categorylist) {
+                     echo "<br>".$categorylist;
+                      $i++;
+              }
+          }
+          dd("bitti");
+  */
+        header("Content-Type: application/xml; charset=utf-8");
+        $simplexml = new SimpleXMLElement('<?xml version="1.0"  encoding="UTF-8"?><products/>');
+
+        $result = Product::all();
+        foreach ($result as $items) {
+            $content = ProductContent::where('product_id', $items->product_id)->where('language_id', 1)->first()->name;
+            $book1 = $simplexml->addChild('product');
+            $book1->addChild("product_id", $items->id);
+            $book1->addChild("active", 1);
+            $book1->addChild("name", '<![CDATA[' . html_entity_decode(ProductContent::where('product_id', $items->product_id)->where('language_id', 1)->first()->name) . ' ]]');
+            $book1->addChild("description", '<![CDATA[' . html_entity_decode($content) . ' ]]');
+            $book1->addChild("product_code", $items->product_id);
+            $book1->addChild("barcode", $items->product_id);
+            $book1->addChild("brand", "akuzun");
+            $book1->addChild("stock", 5);
+            $book1->addChild("cost_price", 0);
+            $book1->addChild("cost_price_supplier", 0);
+            $book1->addChild("currency", "TRY");
+            $book1->addChild("price", $items->price + ($items->price * 100) / 100);
+            $book1->addChild("images", 'http://akuzun.ml/storage/' . ($items->product_id . ".jpg"));
+            $book1->addChild("variants", '');
+            $category_data = $book1->addChild("category_data");
+            $categorytree = $category_data->addChild('category_path');
+            $categorylists = explode("&", $items->category_tree);
+            $i = 1;
+            foreach ($categorylists as $categorylist) {
+                $categorytree->addChild('category_'.$i.'', $categorylist);
+                $i++;  if ($i == 3) {
+                    break;
+                }
+            }
+            $category_data->addChild('category_id');
+        }
+
+
+        $resultO = XmlProduct::all();
+
+        foreach ($resultO as $itemso) {
+            $book2 = $simplexml->addChild('product');
+            $book2->addChild("product_id", $itemso->ProductId);
+            $book2->addChild("active", 1);
+            $book2->addChild("name", '<![CDATA[' . html_entity_decode($itemso->ProductName) . ' ]]');
+            $book2->addChild("description", '<![CDATA[' . html_entity_decode($itemso->FullDescription) . ' ]]');
+            $book2->addChild("product_code", $itemso->ProductSku);
+            $book2->addChild("barcode", $itemso->ProductGtin);
+            $book2->addChild("stock", $itemso->ProductStockQuantity);
+            $book2->addChild("price", $itemso->ProductPrice + ($itemso->ProductPrice * 100) / 100);
+            $book2->addChild("cost_price", 0);
+            $book2->addChild("cost_price_supplier", 0);
+            $book2->addChild("currency", "TRY");
+
+            $x = XmlPictures::where('ProductId', $itemso->ProductId)->get();
+
+            if ($x) {
+                foreach ($x as $image) {
+                    $picturesII[] = $image->PictureUrl;
+                }
+                $picturesI = implode(",", $picturesII);
+
+                $book2->addChild("images", $picturesI);
+            } else {
+                $book2->addChild("images", '');
+            }
+
+
+            $varyantarray = XmlProductCombination::where('ProductId', $itemso->ProductId)->get();
+            $varyants = $book2->addChild("varyants");
+
+            foreach ($varyantarray as $varyant) {
+                $varyantItem = $varyants->addChild('variant');
+                $varyantItem->addChild('variant_id', $varyant->ProductCombinationId);
+                $varyantItem->addChild('barcode', $varyant->VariantGtin);
+                $varyantItem->addChild('product_code', $varyant->ProductId);
+                $varyantItem->addChild('stock', $varyant->VariantStockQuantity);
+                $varyantItem->addChild('price', $itemso->ProductPrice + ($itemso->ProductPrice * 100) / 100);
+                $varyantItem->addChild('cost_price', 0);
+                $varyantItemAttributes = $varyantItem->addChild('options');
+                $varyantItemAttribute = $varyantItemAttributes->addChild('option1');
+                $varyantItemAttribute->addChild('name', $varyant->VariantName);
+                $varyantItemAttribute->addChild('value', $varyant->VariantValue);
+            }
+
+            $manufacturer = XmlManufacturer::where('ProductId', $itemso->ProductId)->first();
+
+            $category_data = $book2->addChild("category_data");
+            $categorytree = $category_data->addChild('category_path');
+            $categorytree->addChild('category_1', $itemso->Breadcrumb);
+            $category_data->addChild('category_id');
+            $book2->addChild("brand", $manufacturer->ManufacturerName ?? 'Akuzun');
         }
 
         file_put_contents('books.xml', $simplexml->asXML());
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\XmlController;
-use App\Models\Xml;
+use App\Models\xml;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,11 +15,42 @@ use App\Models\Xml;
 |
 */
 
+/*
 Route::get('/', function () {
-    
+
    $data['xml'] =  Xml::all();
     return view('welcome',$data);
 });
+*/
 Route::post('xml.save', [XmlController::class, 'save'])->name('xml.save');
 Route::get('xml.parser', [XmlController::class, 'parser'])->name('xml.parser');
+Route::get('xml.databaseXml', [XmlController::class, 'databaseXml'])->name('xml.databaseXml');
 
+
+Route::get('/category', [\App\Http\Controllers\Trendyol\IndexController::class,'categoryUrlCrawler']);
+//Route::get('/products/{pi}', [\App\Http\Controllers\Trendyol\IndexController::class,'allProducts']);
+Route::get('/prod', [\App\Http\Controllers\Trendyol\IndexController::class,'allProducts']);
+Route::get('/json/{pi}', [\App\Http\Controllers\Trendyol\IndexController::class,'jsonSave']);
+Route::get('/json', [\App\Http\Controllers\Trendyol\IndexController::class,'jsonSave']);
+Route::get('/view', [\App\Http\Controllers\Trendyol\IndexController::class,'view']);
+
+Route::get('/joom-send', [\App\Http\Controllers\Joom\IndexController::class,'send']);
+
+
+Route::get('/xml-products-save', [\App\Http\Controllers\XmlProduct\indexController::class,'index']);
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['auth'])->name('dashboard');
+
+    Route::resource('/seller', \App\Http\Controllers\Seller\IndexController::class);
+    Route::get('/category-match', [\App\Http\Controllers\HomeController::class,'categoryMatch'])->name('categoryMatch');
+    Route::get('/product-list', [\App\Http\Controllers\HomeController::class,'productList'])->name('productList');
+});
+
+
+
+
+
+require __DIR__.'/auth.php';
